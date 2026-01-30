ad51e1f6ec568cf3f195de2801f0f85afdaf11ae
Rebeca <Rebeca@DESKTOP-996ACDP>
2026-01-30 13:47:24 +0200

    Add project files.

---
+++ program.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+
+
+
+int main() {
+
+	cout << "Lista de cumparaturi:\n";
+	cout << "Studenta Stancioni Rebeca\n";
+
+
+
+
+
+
+
+	return 0;
+}
