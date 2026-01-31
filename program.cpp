aa058a9bb235d4fcf8947c7b7627857ce70590bd
Rebeca-Emanuela Stăncioni <rebeca-emanuela.stancioni@student.upt.ro>
2026-01-31 15:45:17 +0200

    primul commit: Am creat structura de date pentru articol si meniul help

--- program.cpp
+++ program.cpp
@@ -1,7 +1,66 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
 
 using namespace std;
+
+struct Articol {
+    string nume;
+    int cantitate;
+    double pretUnitar;
+    string categorie;
+};
+
+void printHelp() {
+    cout << "Comenzi disponibile:\n";
+    cout << "  help\n";
+    cout << "  add <nume> <cantitate> <pret> <categorie>\n";
+    cout << "  del <nume>\n";
+    cout << "  list [sort=nume|pret|categorie]\n";
+    cout << "  filter <categorie>\n";
+    cout << "  total\n";
+    cout << "  export <fisier.csv>\n";
+    cout << "  exit\n";
+}
+
+int main() {
+    vector<Articol> articole;
+
+    cout << "Gestionare articole (tasteaza 'help' pentru comenzi)\n";
+
+    string line;
+    while (true) {
+        cout << "\n> ";
+        getline(cin, line);
+
+        if (line.empty()) continue;
+
+        stringstream ss(line);
+        string cmd;
+        ss >> cmd;
+
+        if (cmd == "help") {
+            printHelp();
+        }
+        else if (cmd == "exit") {
+            cout << "La revedere!\n";
+            break;
+        }
+        else {
+            cout << "Comanda necunoscuta. Tasteaza 'help'.\n";
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
 
 
 
